4e4abe86f39b2c29899502987ec435d4b5e18d1d
NikitaNar <58106373+NikitaNar@users.noreply.github.com>
2019-11-26 16:11:40 +0300

    Add files via upload

---
+++ homet/if.py
@@ -0,0 +1,23 @@
+age=input('Введите свой возраст: ')
+age=int(age)
+if age > 21:
+    print('Работаешь')
+elif 18 <= age <= 21:
+    print('В ВУЗе')
+elif 6 <= age <= 17:
+    print('В школе')
+else: print('В садике')
+
+x=input('Введите 1 строку: ')
+y=input('Введите 2 строку: ')
+
+#не сработает
+
+
+if x=1:print('1')
+elif len(x) > len(y):print('2')
+elif y='learn':
+    if x!=y:print("3")
+    else: continue
+else: print("0")
+
